18aa7be76e7ae558981e621f5970527c68ba7778
christiansaccani <christian.saccani99@gmail.com>
2024-04-12 05:22:17 +0200

    exercise done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,26 @@
 @extends('layouts/app')
 
 @section('content')
+
+<div class="container my-5 py-5 px-5">
+
+    <h1>Lista dei Film</h1>
+
+        <div id="everything">
+        @foreach ($movies as $movie)
+
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">{{$movie -> title}}</h5>
+                    <h6 class="card-subtitle mb-2">{{$movie -> original_title}}</h6>
+                    <p class="card-text">{{$movie -> vote}}</p>
+                </div>
+            </div>
+
+        @endforeach
+        </div>
+
+</div>
+
+@endsection
+
